a5fb01aaf8a76eec0810013e5f32cb7ac80f8c6a
Barry Warnock <brrwarnock@gmail.com>
2018-01-17 17:34:58 -0800

    Initial commit

---
+++ tetris.c
@@ -0,0 +1,17 @@
+void print_alphabet() {
+  for (int i = 0; i < 26; i++) {
+    char c = 'A'+i;
+    
+    asm (
+		  "mov %[c], %%al;"
+		  "mov $0x0E, %%ah;"
+		  "int $0x10;"
+		  :
+		  : [c] "r" (c)
+		  );
+  }
+}
+
+extern void loader_main() {
+  print_alphabet();
+}
